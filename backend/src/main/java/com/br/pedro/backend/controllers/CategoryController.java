a9a7327ad1ed7a0deec0b93737b5901c223ae0a2
pedrodanieljardim <pedro.d.jardim@hotmail.com>
2021-05-25 21:13:09 -0300

    Implementando a primeira classe de domínio

--- backend/src/main/java/com/br/pedro/backend/controllers/CategoryController.java
+++ backend/src/main/java/com/br/pedro/backend/controllers/CategoryController.java
@@ -1,16 +1,27 @@
 package com.br.pedro.backend.controllers;
 
+import com.br.pedro.backend.domain.Category;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/category")
 public class CategoryController {
 
     @GetMapping
-    public String allCategories(){
-        return "teste de controller!";
+    public List<Category> allCategories(){
+        Category c1 = new Category(1L, "Informática");
+        Category c2 = new Category(2L, "Escritório");
+
+        List<Category> categoryList = new ArrayList<>();
+        categoryList.add(c1);
+        categoryList.add(c2);
+
+        return categoryList;
     }
 
 }
---
+++ backend/src/main/java/com/br/pedro/backend/domain/Category.java
@@ -0,0 +1,18 @@
+package com.br.pedro.backend.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Category {
+
+    private Long id;
+    private String name;
+
+
+}
