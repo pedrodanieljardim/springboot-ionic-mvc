65e2cd912704698bd6769c1612a90baa9d697649
pedrodanieljardim <pedro.d.jardim@hotmail.com>
2021-05-25 21:35:36 -0300

    Banco de dados H2 de testes e inclusão do javax

--- backend/src/main/java/com/br/pedro/backend/domain/Category.java
+++ backend/src/main/java/com/br/pedro/backend/domain/Category.java
@@ -5,14 +5,20 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
 @Data
 @Builder
+@Entity
 @AllArgsConstructor
 @NoArgsConstructor
+// @Table(name = "category")
 public class Category {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "name")
     private String name;
-
-
 }
