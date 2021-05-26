f5ae0dcedda706b86d05bf58b9bd43ddb6a2cd72
pedrodanieljardim <pedro.d.jardim@hotmail.com>
2021-05-25 23:08:00 -0300

    Operações de Instanciações

--- backend/src/main/java/com/br/pedro/backend/BackendApplication.java
+++ backend/src/main/java/com/br/pedro/backend/BackendApplication.java
@@ -1,13 +1,30 @@
 package com.br.pedro.backend;
 
+import com.br.pedro.backend.domains.Category;
+import com.br.pedro.backend.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class BackendApplication {
+public class BackendApplication implements CommandLineRunner {
+
+    @Autowired
+    CategoryRepository categoryRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+
+        Category cat1 = new Category(null, "Informática");
+        Category cat2 = new Category(null, "Escritório");
+        categoryRepository.saveAll(Arrays.asList(cat1, cat2));
+
+    }
 }
